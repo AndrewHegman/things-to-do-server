ed996c0571fd1d7e61349ab85336959ebad24b3e
Andrew Hegman <arhegman@gmail.com>
2021-08-06 16:32:43 -0400

    Adding nodemon

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,8 @@ var app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
